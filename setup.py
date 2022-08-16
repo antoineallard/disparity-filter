70bdecb212074f6bbf7b3a9855bc60a6e3164ced
Malcolm van Raalte <malcolm@van.raalte.ca>
2022-08-16 16:31:49 -0400

    1.1.2 - support for more python versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'backbone_network',         # How you named your package folder (MyLib)
   packages = ['backbone_network'],   # Chose the same as "name"
-  version = '1.1.1',      # Start with a small number and increase it with every change you make
+  version = '1.1.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Extracts the multiscale backbone of complex weighted networks',   # Give a short description about your library
   author = 'Malcolm van Raalte',                   # Type in your name
@@ -21,5 +21,9 @@ setup(
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
   ],
 )
