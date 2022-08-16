2e8a9254669241094a481ee00dc01ef64bd03704
Malcolm van Raalte <malcolm@van.raalte.ca>
2022-08-16 16:32:19 -0400

    1.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'backbone_network',         # How you named your package folder (MyLib)
   packages = ['backbone_network'],   # Chose the same as "name"
-  version = '1.1.2',      # Start with a small number and increase it with every change you make
+  version = '1.1.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Extracts the multiscale backbone of complex weighted networks',   # Give a short description about your library
   author = 'Malcolm van Raalte',                   # Type in your name
